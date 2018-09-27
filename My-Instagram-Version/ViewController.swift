1ba7bffc3fb0cb95cb7a2ee884487bb199b7cfbf
Luis Mendez <16315708+LuisMendez01@users.noreply.github.com>
2018-09-27 01:06:13 -0400

    Initial Commit

---
+++ My-Instagram-Version/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My-Instagram-Version
+//
+//  Created by Silvia L Mendez on 9/27/18.
+//  Copyright © 2018 Luis Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
