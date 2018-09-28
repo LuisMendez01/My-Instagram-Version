8d19e381b34ed3169f6daacdbcf0ade4b7201b61
Luis Mendez <16315708+LuisMendez01@users.noreply.github.com>
2018-09-28 15:54:27 -0400

    Added Assets.xcassets

--- My-Instagram-Version/Controllers/LoginViewController.swift
+++ My-Instagram-Version/Controllers/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var messageField: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
